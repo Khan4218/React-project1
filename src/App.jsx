c52b86a04a78c87cf5fe83dc0920df70e6f7cbb5
Khan4218 <mirakber4@gmail.com>
2025-05-26 18:56:06 +0530

    Simple static page project 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Navbar } from "./components/Navbar";
+import { Main } from "./components/Main";
+
+export function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+
